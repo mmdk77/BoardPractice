8cccf1ec5b611a6078629341cb96d60ea2e874e3
Seonyong Lee <mmdk77@gmail.com>
2018-05-24 18:39:29 +0900

    Add files via upload

--- SpringBoard_Mybatis_1/WebContent/board/list.jsp
+++ SpringBoard_Mybatis_1/WebContent/board/list.jsp
@@ -1,11 +1,14 @@
+<%@page import="com.spring.board.common.PagingBean"%>
 <%@page import="com.spring.board.domain.Board"%>
 <%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-
+<%! PagingBean pb=new PagingBean();%>
 <%
+
 	List<Board> list = (List<Board>) request.getAttribute("list");
 	out.print(list.size()+"개");
+	pb.initPage(list.size(), request);
 
 %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -32,10 +35,15 @@
 			</tr>
 		</thead>
 		<tbody>
-			<%for(int a=0; a<list.size(); a++) {%>
-			<%Board board = list.get(a); %>
-			<tr align="center" height="20px">
-				<td width="50"><%=board.getBoard_seq() %></td>
+			<%
+				int num=pb.getNum();
+				int curPos = pb.getCurPos();
+			%>
+			<%for(int a=1; a<pb.getPageSize(); a++) {%>
+			<%if(num<1)break; %>
+			<%Board board = list.get(curPos++); %>
+			<tr align="center" height="50px">
+				<td width="50"><%=num--%></td>
 				<td width="300"><a href="/board/<%=board.getBoard_seq()%>"><%=board.getTitle()%></a></td>
 				<td width="100"><%=board.getWriter()%></td>
 				<td width="100"><%=board.getRegdate().substring(0, 10) %></td>
@@ -44,7 +52,21 @@
 			<%} %>
 			<tr>
 				<td height="1" colspan="5" align="center">
-					<a>[1]</a>
+					<%if(pb.getFirstPage()-1<1){ %>
+						<a href="javascript:alert('이전페이지가 없습니다.')">◀</a>
+					<%}else{%>
+						<a href="/board/list?currentPage=<%=pb.getFirstPage()-1%>">◀</a>
+					<% }%>
+					
+					<%for(int i=pb.getFirstPage();i<=pb.getLastPage();i++){ %>
+						<%if(pb.getTotalPage()<i) break;%>
+							<a <%if(pb.getCurrentPage()==i){%> <%} %>href="/board/list?currentPage=<%=i%>">[<%=i %>]</a>
+					<%} %>	
+					<%if(pb.getLastPage()+1>=pb.getTotalPage()){ %>	
+						<a href="javascript:alert('다음페이지가 없습니다.')">▶</a>
+					<%}else{ %>
+						<a href="/board/list?currentPge=<%=pb.getLastPage()+1%>">▶</a>
+					<%} %>
 				</td>
 			</tr>
 		</tbody>
---
+++ SpringBoard_Mybatis_1/src/com/spring/board/common/PagingBean.java
@@ -0,0 +1,125 @@
+package com.spring.board.common;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class PagingBean {
+
+	
+	private int currentPage=1;
+	private int totalRecord;
+	private int pageSize=10;
+	private int totalPage;
+	private int blockSize=10;
+	private int firstPage;
+	private int lastPage;
+	private int curPos;
+	private int num;
+	
+	
+	public void initPage(int totalRecord, HttpServletRequest req) {
+		if(req.getParameter("currentPage")!=null) {
+			currentPage = Integer.parseInt(req.getParameter("currentPage"));
+		}
+		this.totalRecord = totalRecord;
+		totalPage = (int) Math.ceil((float)totalRecord/pageSize);
+		firstPage = currentPage-((currentPage-1)%blockSize);
+		lastPage = firstPage+(blockSize-1);
+		curPos=(currentPage-1)*pageSize;
+		num=totalRecord-curPos;
+		
+	}
+
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+
+
+	public int getTotalRecord() {
+		return totalRecord;
+	}
+
+
+	public void setTotalRecord(int totalRecord) {
+		this.totalRecord = totalRecord;
+	}
+
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+
+	public int getTotalPage() {
+		return totalPage;
+	}
+
+
+	public void setTotalPage(int totalPage) {
+		this.totalPage = totalPage;
+	}
+
+
+	public int getBlockSize() {
+		return blockSize;
+	}
+
+
+	public void setBlockSize(int blockSize) {
+		this.blockSize = blockSize;
+	}
+
+
+	public int getFirstPage() {
+		return firstPage;
+	}
+
+
+	public void setFirstPage(int firstPage) {
+		this.firstPage = firstPage;
+	}
+
+
+	public int getLastPage() {
+		return lastPage;
+	}
+
+
+	public void setLastPage(int lastPage) {
+		this.lastPage = lastPage;
+	}
+
+
+	public int getCurPos() {
+		return curPos;
+	}
+
+
+	public void setCurPos(int curPos) {
+		this.curPos = curPos;
+	}
+
+
+	public int getNum() {
+		return num;
+	}
+
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	
+	
+	
+}
